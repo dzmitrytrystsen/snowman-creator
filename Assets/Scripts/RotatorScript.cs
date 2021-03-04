e75199f253b51734f5ceebf6cf56b57f182bb9d3
dzmitrytrystsen <dzmitrytrystsen@gmail.com>
2021-03-04 17:14:33 +0300

    Script files are added

---
+++ Assets/Scripts/RotatorScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatorScript : MonoBehaviour
+{
+    private float speed = 100f;
+
+    void Update()
+    {
+        RotateTheObject();
+    }
+
+    private void RotateTheObject()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ScalerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScalerScript : MonoBehaviour
+{
+    private float speed = 1.5f;
+    private Vector3 endScale = new Vector3(2f, 2f, 2f);
+
+    void Update()
+    {
+        if(transform.localScale.y < endScale.y)
+            ScaleTheObject();
+    }
+
+    private void ScaleTheObject()
+    {
+        transform.localScale += new Vector3(0.1f, 0.1f, 0.1f) * speed * Time.deltaTime;
+    }
+}
